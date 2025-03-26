4bab2b28da9ba1c0f3dbd2d3b236ba4859aba61a
karutt <karutetto332@gmail.com>
2025-03-26 20:14:09 +0900

    Refactor ClientProvider structure and update import paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import ClientProvider from "@/components/common/ClientProvider";
-
+// import { Provider } from "@/components/ui/provider";
+import ClientProvider from "@/components/ui/client-provider";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
