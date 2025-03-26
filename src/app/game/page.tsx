2c54a8a66d78e2dd28b91df90b902673476ae82c
karutt <karutetto332@gmail.com>
2025-03-26 23:15:10 +0900

    Remove unused SVG files and update metadata for improved SEO and site description

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,4 @@
 import { Game } from "@/features/game/conponents/Game";
-
 export default function GamePage() {
     return <Game />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button, Card, Center } from "@chakra-ui/react";
 
 import Link from "next/link";
-
 export default function Home() {
     return (
         <Center height='100svh' px={3}>
